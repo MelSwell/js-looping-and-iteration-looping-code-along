e7cbc3cf92cfd343093296638883001141943a6a
Seth M Cohen <smcohen82@gmail.com>
2021-04-04 16:45:07 -0400

    commit tree 4394b1c640914f61ce9cca744bc48c893f37cf99

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+  const cards = []
+  for (let i = 0; i < arr.length; i++) {
+    cards.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+  }
+  return cards
+}
+
+function countDown(num) {
+  while (num >= 0){
+    console.log(num)
+    num -= 1
+  }
+}
